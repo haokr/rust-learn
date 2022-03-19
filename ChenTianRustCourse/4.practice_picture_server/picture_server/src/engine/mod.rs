92115656a48bbe4de769504a8daca98b2d78953a
haow <995199838@qq.com>
2022-03-19 16:24:32 +0800

    picture server - add picture process

---
+++ ChenTianRustCourse/4.practice_picture_server/picture_server/src/engine/mod.rs
@@ -0,0 +1,16 @@
+use crate::pb::Spec;
+use image::ImageOutputFormat;
+
+mod photon;
+pub use photon:Phortn;
+
+// Engine trait：未来可以添加更多的 engine，主流程只需要替换 engine
+pub trait Engine {
+    fn apply(&mut self, specs: &[Spec]);
+    fn generate(self, format: ImageOutputFormat) -> Vec<u8>;
+}
+
+// SpecTransform: 未来如果添加更多的Spec，只需要实现它即可
+pub trait SpecTransform<T> {
+    fn transform(&mut self, op: T);
+}
