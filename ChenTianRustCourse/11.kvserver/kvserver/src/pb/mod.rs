a6aa970cdab9be36135144f911b0626524a0e1bf
haow <995199838@qq.com>
2022-06-19 19:02:56 +0800

    kv server

---
+++ ChenTianRustCourse/11.kvserver/kvserver/src/pb/mod.rs
@@ -0,0 +1,40 @@
+pub mod abi;
+
+use abi::{command_request::RequestData, *};
+
+impl CommandRequest {
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            }))
+        }
+    }
+}
+
+impl Kvpair {
+    /// 创建新的 kv pair
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s))
+        }
+    }
+}
+
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        Self { 
+            value: Some(value::Value::String(s.into())),
+        }
+    }
+}
