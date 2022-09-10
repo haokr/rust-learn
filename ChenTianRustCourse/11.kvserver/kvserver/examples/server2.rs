21fc2d59653fe48ae999d16a9d249d74ea7a84fa
haow <995199838@qq.com>
2022-09-10 17:21:48 +0800

    rust log

--- ChenTianRustCourse/11.kvserver/kvserver/examples/server2.rs
+++ ChenTianRustCourse/11.kvserver/kvserver/examples/server2.rs
@@ -7,7 +7,10 @@ use tokio::net::TcpListener;
 use tokio_util::codec::{LengthDelimitedCodec, Framed};
 use tracing::info;
 
-
+/**
+ * 默认日志等级是 WARN
+ * 设置环境变量 RUST_LOG="info" 来展示 info 日志
+ */
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
